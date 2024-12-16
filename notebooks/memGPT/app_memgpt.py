8321369e07edee0ddf7a3346520b359ecdb2716a
Thomas Chang <thomaschang@salesforce.com>
2024-12-16 13:18:24 -0800

    Update streamlit app

--- notebooks/memGPT/app_memgpt.py
+++ notebooks/memGPT/app_memgpt.py
@@ -56,23 +56,38 @@ def chat(agent):
             placeholder.write(agent_response)
 
 
+def delete_agents():
+    if st.button("Delete Agents"):
+        for agent_state in client.list_agents():
+            st.write(f"Deleting agent {agent_state.name}")
+            client.delete_agent(agent_id=agent_state.id)
+
+
+def get_agents():
+    all_agents = []
+    #if st.button(f"List Agents: {header}"):
+    for agent_state in client.list_agents():
+        all_agents.append({'name': agent_state.name, 'id': agent_state.id})
+
+    return all_agents
+
+
 def workflow_openai():
-    with st.expander("**List Agents**"):
-        st.write(list_agents("All Agents"))
+    os.environ['OPENAI_API_KEY'] = 'TBD'
+    os.environ['OPENAI_PROJECT'] = 'TBD'
 
-    with st.expander("**Delete Agents**"):
-        delete_agents()
-
-    with st.expander("**Create New Agent Agent**"):
+    with st.expander("**Create New Agent**"):
         import datetime
         agent_name = st.text_input('Agent Name', value="Jarvis")
         agent_name += "-" + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 
-        agent_persona = st.text_input('Agent Persona', value="You are a helpful assistant that loves emojis")
+        st.divider()
+        st.write('Short Term Memory Modules')
+        agent_persona = st.text_input('Agent:', value="You are a helpful assistant that loves emojis")
 
-        human_name = st.text_input('Human Name', value="Bobby")
+        human_name = st.text_input('User:', value="Bobby")
 
-        if st.button(f'Create Agent'):
+        if st.button(f'Create'):
             client.set_default_llm_config(LLMConfig.default_config("gpt-4o-mini"))
             client.set_default_embedding_config(EmbeddingConfig.default_config("text-embedding-ada-002"))
 
@@ -90,18 +105,38 @@ def workflow_openai():
                 for b in memory_block_names:
                     st.write(memories.get_block(b))
 
+    with st.expander("**List Agents**"):
+        agents_read = get_agents()
+        if agents_read:
+            st.write(agents_read)
+
     with st.expander("**Converse With Agent**"):
-        agent_active_id = st.text_input('Please enter agent id:', value="agent-b01eebd6-2e92-45d5-8672-cb8533a1c4af")
+        agent_ids = [a.get("id") for a in get_agents()]
+
+        #agent_active_id = st.text_input('Please enter agent id:', value="")
+        agent_active_id = st.selectbox("Please select an agent", agent_ids)
 
         if agent_active_id:
             agent_active = client.get_agent(agent_active_id)
+
+            if st.button('**:blue[Show Short Term Memory]**'):
+                memory_block_names = ['persona', 'human']
+                memories_short = client.get_core_memory(agent_active.id)
+                for b in memory_block_names:
+                    st.write(b)
+                    st.write(memories_short.get_block(b))
+
+            if st.button('**:blue[Show Long Term Memory]**'):
+                memories_long = client.get_archival_memory(agent_active.id)
+                for l in memories_long:
+                    st.write(l)
+
+            st.divider()
             chat(agent_active)
 
-            if st.button('Show Memory Of Active Agent'):
-                memory_block_names = ['persona', 'human']
-                memories = client.get_core_memory(agent_active.id)
-                for b in memory_block_names:
-                    st.write(memories.get_block(b))
+    with st.expander("**Delete Agents**"):
+        delete_agents()
+
 
 
 def workflow_ollama():
@@ -111,25 +146,8 @@ def workflow_ollama():
     return True
 
 
-def delete_agents():
-    if st.button("Delete Agents"):
-        for agent_state in client.list_agents():
-            st.write(f"Deleting agent {agent_state.name}")
-            client.delete_agent(agent_id=agent_state.id)
-
-
-def list_agents(header):
-    all_agents = []
-
-    if st.button(f"List Agents: {header}"):
-        for agent_state in client.list_agents():
-            all_agents.append({'name': agent_state.name, 'id': agent_state.id})
-
-    return all_agents
-
-
 def app():
-    st.title("Agents With Memory")
+    st.title("Smart Agents, Smarter Memories")
     task = st.sidebar.radio("Select a LLM", ("OpenAI", "Ollama") )
 
     if task == "Ollama":
